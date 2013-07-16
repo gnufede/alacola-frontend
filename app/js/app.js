08a120c36950131142504fea1641d9536273a084
Fede <gnu.fede@gmail.com>
2013-07-16 15:44:47 +0200

    adding calendar

--- app/js/app.js
+++ app/js/app.js
@@ -2,6 +2,33 @@
 
 /* App Module */
 
+angular.module('alacola', ['ui.bootstrap']);
+var DatepickerDemoCtrl = function ($scope) {
+  $scope.today = function() {
+    $scope.dt = new Date();
+  };
+  $scope.today();
+
+  $scope.showWeeks = false;
+  $scope.toggleWeeks = function () {
+    $scope.showWeeks = ! $scope.showWeeks;
+  };
+
+  $scope.clear = function () {
+    $scope.dt = null;
+  };
+
+  // Disable weekend selection
+  $scope.disabled = function(date, mode) {
+    return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
+  };
+
+  $scope.toggleMin = function() {
+    $scope.minDate = ( $scope.minDate ) ? null : new Date();
+  };
+  $scope.toggleMin();
+};
+
 angular.module('alacola', ['alacolaFilters']).
   config(['$routeProvider', function($routeProvider) {
   $routeProvider.
